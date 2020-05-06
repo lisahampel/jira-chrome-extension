39a5a6d33418f89989289dabe2b68a92efb995e6
Lisa <l.hampel@bynary.io>
2020-05-06 07:59:54 +0200

    add screenshot service

--- src/app/services/screenshot.service.ts
+++ src/app/services/screenshot.service.ts
@@ -3,18 +3,21 @@ import {Injectable} from '@angular/core';
 @Injectable({
     providedIn: 'root'
 })
-
 export class ScreenshotService {
 
     constructor() {
     }
 
-    takeScreenshot() {
-        chrome.tabs.captureVisibleTab((screenshotUrl) => {
-            const link = document.createElement('a');
-            link.download = 'screenshot.png';
-            link.href = screenshotUrl;
-            link.click();
+    takeScreenshot(): Promise<string> {
+        return new Promise((resolve) => {
+            chrome.tabs.captureVisibleTab(resolve);
         });
+    }
+
+    downloadUrl(screenshotUrl) {
+        const link = document.createElement('a');
+        link.download = 'screenshot.png';
+        link.href = screenshotUrl;
+        link.click();
     }
 }
